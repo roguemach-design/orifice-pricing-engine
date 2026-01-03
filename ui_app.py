d55413f5ef70e36738ce5e4271bc4daed34a23e8
roguemach-design <roguemach@gmail.com>
2026-01-02 22:11:49 -0500

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -22,7 +22,7 @@ st.markdown(
         padding-top: 0.5rem !important;
         max-width: 1400px;
         padding-left: 2.5rem;
-        padding-right: 2.5rem;
+        padding-right: 1.5rem;
         margin-left: auto;
         margin-right: 10px;
     }
@@ -465,6 +465,7 @@ with right:
                     "ships_in_days": int(ships_in_days),
                 }
                 start_checkout(payload_inputs)
+
 
 
 
